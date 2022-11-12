b7ea0edf33c845092ec0dfc306c2a0b56ebf4126
megumii <98658943+Megumiiiiii@users.noreply.github.com>
2022-11-13 02:45:52 +0700

    Create mvc.sh

---
+++ mvc.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+clear
+echo "╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬"
+echo -e "\033[0;35m"
+echo " ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░                                                                     ";
+echo " ░░░░░████░░░░░░░░░░░░░░░████░░░░░                                                                     ";
+echo " ░░░░███░░░░░░░░░░░░░░░░░░░███░░░░                                                                     ";
+echo " ░░░███░░░░░░░░░░░░░░░░░░░░░███░░░                                                                     ";
+echo " ░░███░░░░░░░░░░░░░░░░░░░░░░░███░░                                                                     ";
+echo " ░███░░░░░░░░░░░░░░░░░░░░░░░░░███░                                                                     ";
+echo " ████░░░░░░░░░░░░░░░░░░░░░░░░░████                                                                     ";
+echo " ████░░░░░░░░░░░░░░░░░░░░░░░░░████                                                                     ";
+echo " ██████░░░░░░░███████░░░░░░░██████                                                                     ";
+echo " █████████████████████████████████                                                                     ";
+echo " █████████████████████████████████                                                                     ";
+echo " ░███████████████████████████████░                                                                     ";
+echo " ░░████░███████████████████░████░░                                                                     ";
+echo " ░░░░░░░███▀███████████▀███░░░░░░░ ██████╗░░█████╗░  ███╗░░██╗░█████╗░██████╗░███████╗                 ";
+echo " ░░░░░░████──▀███████▀──████░░░░░░ ██╔══██╗██╔══██╗  ████╗░██║██╔══██╗██╔══██╗██╔════╝                 ";
+echo " ░░░░░░█████───█████───█████░░░░░░ ██████╦╝██║░░╚═╝  ██╔██╗██║██║░░██║██║░░██║█████╗░░                 ";
+echo " ░░░░░░███████▄█████▄███████░░░░░░ ██╔══██╗██║░░██╗  ██║╚████║██║░░██║██║░░██║██╔══╝░░                 ";
+echo " ░░░░░░░███████████████████░░░░░░░ ██████╦╝╚█████╔╝  ██║░╚███║╚█████╔╝██████╔╝███████╗                 ";
+echo " ░░░░░░░░█████████████████░░░░░░░░ ╚═════╝░░╚════╝░  ╚═╝░░╚══╝░╚════╝░╚═════╝░╚══════╝                 ";
+echo " ░░░░░░░░░███████████████░░░░░░░░░                                                                     ";
+echo " ░░░░░░░░░░█████████████░░░░░░░░░░                                                                     ";
+echo " ░░░░░░░░░░░███████████░░░░░░░░░░░                                                                     ";
+echo " ░░░░░░░░░░███──▀▀▀──███░░░░░░░░░░                                                                     ";
+echo " ░░░░░░░░░░███─█████─███░░░░░░░░░░                                                                     ";
+echo " ░░░░░░░░░░░███─███─███░░░░░░░░░░░                                                                     ";
+echo " ░░░░░░░░░░░░█████████░░░░░░░░░░░░                                                                     ";
+echo " ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░                                                                     ";
+echo -e "\e[0m"
+
+echo "╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬"
+
+echo "╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬"
+
+echo -e '\e[33mProject =\e[37m' MVC
+echo -e '\e[33mCommunity =\e[37m' BeritaCryptoo
+echo -e '\e[33mDiscord =\e[37m' https://discord.gg/beritacryptoonode
+echo -e '\e[33mTelegram Channel =\e[37m' https://t.me/BeritaCryptoo
+echo -e '\e[33mTelegram Group =\e[37m' https://t.me/BeritaaCryptooGroup
+echo -e '\e[33mTwitter =\e[37m' https://twitter.com/BeritaCryptoo
+echo -e "\e[0m"
+
+echo "╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬"
+
+
+
+sleep 2
+
+echo -e "\e[1m\e[32m1. Install packages... \e[0m" && sleep 1
+# update
+cd $HOME
+sudo apt update && sudo apt install screen && sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils && sudo apt-get install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev && sudo apt-get install libdb-dev && sudo apt-get install libdb++-dev && sudo apt-get install libczmq-dev
+
+echo -e "\e[1m\e[32m4. Download bahan... \e[0m" && sleep 1
+wget https://raw.githubusercontent.com/heshuchao/space-mining-guide/main/mvc.conf
+wget https://raw.githubusercontent.com/heshuchao/space-mining-guide/main/install-node.sh && chmod 777 chmod 777 ./install-node.sh
+
+echo '╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬ </SUDAH> ╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬'
